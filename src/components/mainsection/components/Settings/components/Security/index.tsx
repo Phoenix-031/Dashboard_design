04e54c1e0984fa28899fdf1204d02a6cd9727ed6
Debayan Pradhan <pradhandebayan@gmail.com>
2023-12-24 19:48:57 +0530

    base responsive design

--- src/components/mainsection/components/Settings/components/Security/index.tsx
+++ src/components/mainsection/components/Settings/components/Security/index.tsx
@@ -6,6 +6,8 @@ import { AiOutlineStop } from "react-icons/ai";
 import { IoLocation } from "react-icons/io5";
 import { FaUserFriends } from "react-icons/fa";
 
+import { RiChatPrivateFill } from "react-icons/ri";
+import { MdOutlineRemoveRedEye } from "react-icons/md";
 
 import { Switch } from 'antd';
 
@@ -19,6 +21,7 @@ const SecuriyComponent = () => {
     
   return (
     <div className={styles.security_maincontainer}>
+
         <div className={styles.security_sectionone}>
             <div className={styles.security_card_typeone}>
                 <div>
@@ -72,6 +75,57 @@ const SecuriyComponent = () => {
                 </div>
             </div>
         </div>
+
+        <div className={styles.security_sectiontwo}>
+            <div className={styles.passwordlength_card}>
+                <div>
+                    <FaLock />
+                </div>
+                <div className={styles.passwordlength_section}>
+                    <p>Password length</p>
+                    <p>Password length not less than...</p>
+                </div>
+                <div>
+                    8
+                </div>
+            </div>
+
+            <div className={styles.privatemessage_card}> 
+                <div>
+                    <RiChatPrivateFill />
+                </div>
+
+                <div className={styles.privatemessage_card_section}>
+                    <p>Private messages</p>
+                    <p>Who can send private message</p>
+
+                    <div>
+                        <p>Team</p>
+                        <p>Nobody</p>
+                        <p>Choose</p>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className={styles.visibility_card}>
+                <div>
+                    <MdOutlineRemoveRedEye />
+                </div>
+                <div className={styles.visibility_card_section}>
+                    <p>Visibility</p>
+                    <p>See all profiles except..</p>
+
+                <div>
+                    <p>Emma bron</p>
+                    <p>David jhonson</p>
+                </div>
+                </div>
+            </div>
+
+        </div>
+
     </div>
   )
 }
