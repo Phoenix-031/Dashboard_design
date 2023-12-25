beeff5e2d0bc5dc9743858fd28ecdd811549f250
Debayan Pradhan <pradhandebayan@gmail.com>
2023-12-25 21:52:24 +0530

    added backdrop

--- src/components/sidesection/index.tsx
+++ src/components/sidesection/index.tsx
@@ -9,6 +9,8 @@ import { TbCardsFilled } from "react-icons/tb";
 import { IoSettings } from "react-icons/io5";
 import { IoAddCircle } from "react-icons/io5";
 import { CiLogout } from "react-icons/ci";
+import { GrTechnology } from "react-icons/gr";
+import { FaStar } from "react-icons/fa";
 
 
 
@@ -18,6 +20,11 @@ import { useContext } from 'react';
 
 // assets
 import userIcon from '@assets/profile.jpeg'
+import profile1 from '@assets/profile1.png'
+import profile2 from '@assets/profile2.png'
+import profile3 from '@assets/profile3.png'
+import profile5 from '@assets/profile5.png'
+
 import { ToggleContext, ToggleContextType } from '@/context/ToggleContext';
 
 //interface
@@ -50,6 +57,7 @@ const SideSection = () => {
         <div className={`${styles.sidebar_container} 
                          ${toggle ? styles['toggle']: ''}`
                         }>
+            {/* <Backdrop /> */}
 
             <SidebarHeading onLogout={() => setToggle(false)} />
             <UserInfo userName="Catherine Reed" role="Admin" userIcon={userIcon} />
@@ -71,8 +79,12 @@ const SideSection = () => {
 
 const SidebarHeading = ({ onLogout } : SidebarHeadingProps) => (
   <div className={styles.sidebar_heading}>
-    <img src="" alt="" />
+    <div>
+    <div>
+      <GrTechnology />
+    </div>
     <span>TechHazel</span>
+    </div>
     <p onClick={onLogout}>
       <CiLogout />
     </p>
@@ -85,7 +97,10 @@ const UserInfo = ({ userName, role, userIcon } : UserInfoProps) => (
       <img src={userIcon} alt="User image" />
     </div>
     <span>{userName}</span>
-    <span>{role}</span>
+    <span>
+      <span><FaStar /></span>
+      {role}
+    </span>
   </div>
 );
 
@@ -106,18 +121,35 @@ const SidebarMenuItem = ({ icon, label, setSelected,selected, toggle,setToggle }
   </li>
 );
 
-const SidebarCommunitySection = () => (
+const SidebarCommunitySection = () => {
+  const images = [profile1, profile2, profile3, profile5];
+  return(
   <div className={styles.sidebar_community_section_container}>
     <div className={styles.sidebar_community_members}>
-      {[...Array(4)].map((_, index) => (
-        <img key={index} src={userIcon} alt={`User ${index + 1}`} />
-      ))}
+      {
+      images.map((image,index) => (
+        <img key={index} src={image} alt={`User ${index + 1}`} />
+      ))
+      }
       <div>
         <IoAddCircle />
       </div>
     </div>
     <p className={styles.sidebar_community_section_text}>Find new members in our community</p>
   </div>
-);
+)};
+
+// export const Backdrop: React.FC = () => {
+//   const { toggle, setToggle } = useContext(ToggleContext) as ToggleContextType;
+
+//   const handleBackdropClick = () => {
+//     if (toggle) {
+//       setToggle(false);
+//     }
+//   };
+
+//   return toggle ? <div className={backdropstyle.backdrop} onClick={handleBackdropClick}></div> : null;
+// };
+
 
 export default SideSection
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,14 @@
+import { useContext } from 'react'
+
 //components
 import MainSection from '@/components/mainsection'
 import SideSection from '@/components/sidesection'
 
 //styelesheet
 import styles from './style.module.scss'
+import backdropstyle from './backdrop.module.scss'
+
+import { ToggleContext, ToggleContextType } from '@/context/ToggleContext'
 
 //context provider
 import ToggleContextProvider from '@/context/ToggleContext'
@@ -14,9 +19,24 @@ const Dashboard = () => {
       <ToggleContextProvider>
           <SideSection />
           <MainSection />
+          <Backdrop />
       </ToggleContextProvider>
     </div>
   )
 }
+
+const Backdrop = () => {
+  const { toggle, setToggle } = useContext(ToggleContext) as ToggleContextType;
+
+  const handleBackdropClick = () => {
+    if (toggle) {
+      setToggle(false);
+    }
+  };
+
+  return toggle ? <div className={backdropstyle.backdrop} onClick={handleBackdropClick}></div> : null;
+};
+
+
 
 export default Dashboard
